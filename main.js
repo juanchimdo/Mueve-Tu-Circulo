2c79e371b0cf4c800b267ade43cfe9cfe6328edf
juanchimdo <64117883+juanchimdo@users.noreply.github.com>
2020-06-28 19:15:33 -0300

    funciona en todas direcciones con bordes

--- main.js
+++ main.js
@@ -1,45 +1,57 @@
 document.addEventListener("DOMContentLoaded", function() {
-	const circulo = document.querySelector ('.circulo')
 
-	let posicionHorizontal = -40
-	let posicionVertical = -40
-
-	function moverIzquierda () {
-		posicionHorizontal = posicionHorizontal-20
-		circulo.style.marginLeft = posicionHorizontal + 'px'
-		console.log(circulo.style.marginLeft)
-	}
-	function moverArriba () {
-		posicionVertical = posicionVertical-20
-		circulo.style.marginTop = posicionVertical + 'px'
-	}
-	function moverDerecha () {
-		posicionHorizontal = posicionHorizontal+20
-		circulo.style.marginLeft = posicionHorizontal + 'px'
-	}
-	function moverAbajo () {
-		posicionVertical = posicionVertical+20
-		circulo.style.marginTop = posicionVertical + 'px'
-	}
-
+	const circulo = document.querySelector('.circulo')
+	const container = document.querySelector('.container')
 
 	document.addEventListener('keydown', function (event) {
-	const codigo = event.keyCode
-	if (codigo===37) {
-		return moverIzquierda ()
-	}
-	if (codigo===38) {
-		return moverArriba ()
-	}
-	if (codigo===39) {
-		return moverDerecha ()
-	}
-	if (codigo===40) {
-		return moverAbajo ()
-	}
+		const codigo = event.keyCode
+		if (codigo===37) {							// IZQUIERDA
+			let movx = 0
+			const diametro = circulo.offsetWidth
+			const ancho = container.offsetWidth
+			movx = ancho/diametro
+			const izq = circulo.offsetLeft
+			const valor = izq - movx
+			if (izq>=movx) {
+				circulo.style.left = valor + 'px'
+			}
+		}
+		if (codigo===38) {							// ARRIBA
+			let movy = 0
+			const diametro = circulo.offsetWidth
+			const alto = container.offsetHeight
+			movy = alto/diametro
+			const arriba = circulo.offsetTop
+			const valor = arriba - movy
+			if (arriba>=movy) {
+				circulo.style.top = valor + 'px'
+			}else{
+				circulo.style.top= '0px'
+			}
+		}
+		if (codigo===39) {							// DERECHA
+			let movx = 0
+			const diametro = circulo.offsetWidth
+			const ancho = container.offsetWidth
+			movx = ancho/diametro
+			const izq = circulo.offsetLeft
+			const valor = izq + movx
+			if (valor<ancho - diametro-movx) {
+				circulo.style.left = valor + 'px'
+			}
+		}
+		if (codigo===40) {							// ABAJO
+			let movy = 0
+			const diametro = circulo.offsetWidth
+			const alto = container.offsetHeight
+			movy = alto/diametro
+			const arriba = circulo.offsetTop
+			const valor = arriba + movy
+			if (arriba<alto - diametro - 24 - movy) {
+				circulo.style.top = valor + 'px'
+			}else{
+				circulo.style.top = alto - diametro - 24 + 'px'
+			}
+		}
 	})
 })
-
-function mover () {
-
-}
